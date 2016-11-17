0f5bfcfd93f2b63160766b6b852a69cff3535e79
Vinoth Kannan <vinothkannan@vinkas.com>
2016-11-17 23:08:14 +0530

    rename: brand-header to branding

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
-# name: brand-header
+# name: branding
 # about: Plugin to add a custom brand header for Discourse
 # version: 0.0.1
 # authors: Vinoth Kannan (vinothkannan@vinkas.com)
-# url: https://github.com/vinkas0/discourse-brand-header
+# url: https://github.com/vinkas0/discourse-branding
 
 register_asset 'stylesheets/brand-header.scss'
 register_asset "javascripts/brand-header.js"
