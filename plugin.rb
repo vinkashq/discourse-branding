0ce28ddb89be130e9c300705c8937a7edb94dcd5
Vinoth Kannan <vinothkannan@vinkas.com>
2016-11-18 13:11:49 +0530

    new: settings for branding enabled and url

--- plugin.rb
+++ plugin.rb
@@ -4,5 +4,7 @@
 # authors: Vinoth Kannan (vinothkannan@vinkas.com)
 # url: https://github.com/vinkas0/discourse-branding
 
+enabled_site_setting :branding_enabled
+
 register_asset 'stylesheets/branding.scss'
 register_asset "javascripts/branding.js"
