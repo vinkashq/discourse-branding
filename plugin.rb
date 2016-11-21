0f20e3bd578f2f6b589f802104c7b8e995e9ae0b
Vinoth Kannan <vinothkannan@vinkas.com>
2016-11-21 18:42:29 +0530

    site_title updated in ruby side

--- plugin.rb
+++ plugin.rb
@@ -11,11 +11,27 @@ register_asset "javascripts/branding.js"
 
 after_initialize do
   ApplicationController.class_eval do
+    alias_method :discourse_set_layout, :set_layout
+
     def set_layout
-      if use_crawler_layout?
-        'crawler'
+      if SiteSetting.branding_enabled
+        if use_crawler_layout?
+          'crawler'
+        else
+          File.expand_path('../app/views/layouts/application.html.erb', __FILE__)
+        end
       else
-        File.expand_path('../app/views/layouts/application.html.erb', __FILE__)
+        discourse_set_layout
+      end
+    end
+  end
+
+  ApplicationHelper.module_eval do
+    def site_title
+      if SiteSetting.branding_enabled
+        SiteSetting.brand_name + ' ' + SiteSetting.title
+      else
+        SiteSetting.title
       end
     end
   end
