af3dcadf16dadcdc6c68e8ed8fd89982e12ab402
Vinoth Kannan <vinothkannan@vinkas.com>
2016-11-21 17:38:43 +0530

    brand-header now moved to server-plugin-outlet

--- plugin.rb
+++ plugin.rb
@@ -8,3 +8,15 @@ enabled_site_setting :branding_enabled
 
 register_asset 'stylesheets/branding.scss'
 register_asset "javascripts/branding.js"
+
+after_initialize do
+  ApplicationController.class_eval do
+    def set_layout
+      if use_crawler_layout?
+        'crawler'
+      else
+        File.expand_path('../app/views/layouts/application.html.erb', __FILE__)
+      end
+    end
+  end
+end
